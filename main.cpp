92f364e4d3c5144f4820d6407f52e0888eb77ae4
Petru Marginean <petru.marginean@gmail.com>
2019-07-10 23:25:04 -0400

    misc

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 struct Tiles
 {
     Tiles(int stripes, int length) {}
-;}
+};
 
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
     if (argc != 3)
     {
         std::cerr << "Please provide the number of stripes and length (e.g. 11 30)\n";
         return -1;
     }
-    Tiles tiles(std::stoi(argv[1]), stoi(argv[2]));
+    Tiles tiles(std::stoi(argv[1]), std::stoi(argv[2]));
     return 0;
 }
