92b951c4b5328efd9fa19a34709192c83862d721
Petru Marginean <petru.marginean@gmail.com>
2019-07-10 23:34:39 -0400

    misc

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 struct Tiles
 {
     Tiles(int s, int l) : stripes(s), length(l) {}
-
+    void design();
+    int getDesignCount() const {return designCount; }
 private:
     int stripes;
     int length;
     int designCount = 0;
 };
+
+void Tiles::design()
+{
+}
 
 int main(int argc, char* argv[])
 {
@@ -18,5 +23,7 @@ int main(int argc, char* argv[])
         return -1;
     }
     Tiles tiles(std::stoi(argv[1]), std::stoi(argv[2]));
+    tiles.design();
+    std::cout << "There were " << tiles.getDesignCount() << std::endl;
     return 0;
 }
