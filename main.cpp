bdd71d03b940e9894f90dbad15af6c581d33e740
Petru Marginean <petru.marginean@gmail.com>
2019-07-10 23:36:55 -0400

    misc

--- main.cpp
+++ main.cpp
@@ -22,8 +22,9 @@ int main(int argc, char* argv[])
         std::cerr << "Please provide the number of stripes and length (e.g. $> tiles 11 30)\n";
         return -1;
     }
-    Tiles tiles(std::stoi(argv[1]), std::stoi(argv[2]));
+    int stripes = std::stoi(argv[1]), length = std::stoi(argv[2]);
+    Tiles tiles(stripes, length);
     tiles.design();
-    std::cout << "There were " << tiles.getDesignCount() << std::endl;
+    std::cout << "There were " << tiles.getDesignCount() << " designs for " << stripes << " x " << length << " room." << std::endl;
     return 0;
 }
