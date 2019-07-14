d6d1438b494625657a47ed8f119c6f7c45f90aa5
Petru Marginean <petru.marginean@gmail.com>
2019-07-14 12:21:44 -0400

    misc

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 #g++ main.cpp -std=c++11 -DDEBUG -g -Wall -Wextra -o tiles
 #g++ stripes.cpp -std=c++11 -Wall -Wextra -o stripes 
 #g++ designs.cpp -std=c++11 -Wall -Wextra -o designs
-g++ fastdesigns.cpp -std=c++11 -Wall -Wextra -o fastdesigns
+#g++ fastdesigns.cpp -std=c++11 -Wall -Wextra -o fastdesigns
+g++ dp-designs.cpp -std=c++11 -Wall -Wextra -o dp-designs
---
+++ dp-designs.cpp
@@ -0,0 +1,102 @@
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <cassert>
+
+// $> ./fastdesigns 7 < stripes.txt
+
+#define LOG std::cerr << __FUNCTION__ << std::endl
+
+struct Designs
+{
+    Designs(int s);
+    void load();
+    void run();    
+    void solve(int i, int level);
+    void setCompatible();
+private:
+    const int stripes;
+    std::vector<int> v;
+    std::vector<std::vector<int>> compatible;  
+    std::size_t count = 0;
+};
+
+Designs::Designs(int s) : stripes(s)
+{
+    LOG;
+}
+
+void Designs::setCompatible()
+{
+    LOG;
+    for (std::size_t i = 0; i != v.size(); ++i)
+    {
+        std::vector<int> c;
+        for (std::size_t n = 0; n != v.size(); ++n)
+        {
+            if ((v[i] & v[n]) == 0b100000000000000000000000000000)
+            {
+                c.push_back(n);
+            }
+        }
+       compatible.push_back(c);
+       //std::cerr << "the stripe = " << s << " is compatible with " << c.size() << " other stripes\n";
+    }
+    LOG;
+}
+
+void Designs::load()
+{
+	int n;
+	while(std::cin >> n)
+	{
+	  	v.push_back(n);
+		std::string s;
+		std::cin >> s;
+	}
+	std::cerr << "input size = " << v.size() << "\n";
+}
+
+void Designs::solve(int i, int level)
+{
+    if (level == 0)
+    {
+        ++count;
+        return;
+    }
+    for (auto n: compatible[i])
+    {
+        solve(n, level - 1);
+    }
+}
+
+void Designs::run()
+{
+    LOG;
+    for (size_t i = 0; i != v.size(); ++i)
+    {
+        solve(i, stripes - 1);
+    }
+	std::cerr << "design count = " << count << "\n";    
+}
+
+int main(int argc, char* argv[])
+{
+    auto start = std::chrono::system_clock::now();
+    std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+    std::cout << "start at " << std::ctime(&start_time);
+    assert(argc == 2);
+    int stripes = std::stoi(argv[1]);
+    Designs d(stripes);
+    d.load();
+    d.setCompatible();
+    d.run();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);   
+    std::cout << "start at " << std::ctime(&start_time);
+    std::cout << "end at " << std::ctime(&end_time);
+    std::cout << "elapsed " << elapsed_seconds.count() << " seconds for computing " << stripes << " stripes\n";
+	return 0;
+}
